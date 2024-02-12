cedc9496004cdc2e2bf8df750adfe3eb6fdd6fb5
Willian Gonçalves <willian.p.g@outlook.com>
2024-02-12 17:27:56 -0300

    Requisito 03 - Implemente a função process no módulo file_process. Essa função deverá ser capaz de transformar o conteúdo da lista gerada pela função txt_importer em um dicionário que será armazenado dentro da Queue

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from .file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    for file in instance._items:
+        if file["nome_do_arquivo"] == path_file:
+            print(f"Arquivo {path_file} já processado")
+            return
+
+    data = txt_importer(path_file)
+    if data is not None:
+        file_data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(data),
+            "linhas_do_arquivo": data
+        }
+        instance.enqueue(file_data)
+        print(file_data)
 
 
 def remove(instance):
