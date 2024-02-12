c8a65526801f45153599838b02c3d23118f74bd5
Willian Gonçalves <willian.p.g@outlook.com>
2024-02-12 16:18:11 -0300

    Requisito 02 - Implemente uma função txt_importer dentro do módulo file_management capaz de importar notícias a partir de um arquivo TXT, utilizando "\n" como separador

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            raise ValueError("Formato inválido")
+
+        with open(path_file, "r") as file:
+            return file.read().split('\n')
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+    except ValueError as error:
+        print(str(error), file=sys.stderr)
