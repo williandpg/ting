d643c4d84faf416ace441640b4ddaab3278b460b
Willian Gonçalves <willian.p.g@outlook.com>
2024-02-12 20:51:08 -0300

    Requisito 03 - Corrigindo erros

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -4,8 +4,8 @@ from .file_management import txt_importer
 def process(path_file, instance):
     """Aqui irá sua implementação"""
     for file in instance._queue:
-            if file["nome_do_arquivo"] == path_file:
-                return
+        if file["nome_do_arquivo"] == path_file:
+            return
 
     data = txt_importer(path_file)
     if data is not None:
@@ -18,7 +18,7 @@ def process(path_file, instance):
     instance.enqueue(file_data)
 
     print(file_data)
-    
+ 
 
 def remove(instance):
     """Aqui irá sua implementação"""
