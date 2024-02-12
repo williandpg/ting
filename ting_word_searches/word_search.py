79a0cdfc77d66c69a813f8ac82da4dc56fa13732
Willian Gonçalves <willian.p.g@outlook.com>
2024-02-12 18:24:54 -0300

    Corrigindo erros

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -2,7 +2,7 @@ def exists_word(word, instance):
     """Aqui irá sua implementação"""
     word = word.lower()
     word_list = []
-    for file_data in instance._items:
+    for file_data in instance.queue:
         file_name = file_data["nome_do_arquivo"]
         word_data = []
         for idx, line in enumerate(file_data["linhas_do_arquivo"], start=1):
