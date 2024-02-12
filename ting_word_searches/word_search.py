a31af64af27454d2703b6e3134c1a0530a59150e
Willian Gonçalves <willian.p.g@outlook.com>
2024-02-12 18:47:46 -0300

    Corrigindo erros

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -2,20 +2,22 @@ def exists_word(word, instance):
     """Aqui irá sua implementação"""
     word = word.lower()
     word_list = []
-    for file_data in instance.queue:
-        file_name = file_data["nome_do_arquivo"]
+
+    for i in instance.queue:
+        file_name = i["nome_arquivo"]
         word_data = []
-        for idx, line in enumerate(file_data["linhas_do_arquivo"], start=1):
+        for idx, line in enumerate(i["linhas_do_arquivo"], start=1):
             if word in line.lower():
                 word_data.append({"linha": idx})
+
         if word_data:
             word_list.append({
                 "palavra": word,
                 "arquivo": file_name,
                 "ocorrencias": word_data
             })
-    return word_list
 
+    return word_list
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
