fdeb905c01d0e7ecb69f20f921f65bcda0ac3a67
Willian Gonçalves <willian.p.g@outlook.com>
2024-02-12 21:33:52 -0300

    Requisito 06 - Implemente os testes para a classe PriorityQueue capaz de armazenar arquivos pequenos de forma prioritária

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,19 @@
 # from ting_file_management.priority_queue import PriorityQueue
-
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
 
 def test_basic_priority_queueing():
     """Aqui irá sua implementação"""
+    pq = PriorityQueue()
+    pq.enqueue({"qtd_linhas": 10})
+    pq.enqueue({"qtd_linhas": 1})
+    assert len(pq) == 2
+
+    assert pq.search(0) == {"qtd_linhas": 1}
+    assert pq.search(1) == {"qtd_linhas": 10}
+
+    assert pq.dequeue() == {"qtd_linhas": 10}
+    assert len(pq) == 1
+
+    with pytest.raises(IndexError):
+        pq.search(1)
