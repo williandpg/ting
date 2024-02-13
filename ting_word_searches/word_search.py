38f27da5448e87bd8e73995389b0812cd7606955
Willian Gonçalves <willian.p.g@outlook.com>
2024-02-12 21:18:51 -0300

    Requisito 08 - Implemente uma função search_by_word dentro do módulo word_search, que busque uma palavra em todos os arquivos processados

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -3,7 +3,7 @@ def exists_word(word, instance):
     word_list = list()
 
     for i in instance._queue:
-        file_name = i["nome_arquivo"]
+        file_name = i["nome_do_arquivo"]
         word_data = list()
         for idx, line in enumerate(i["linhas_do_arquivo"], start=1):
             if word.lower() in line.lower():
@@ -21,3 +21,22 @@ def exists_word(word, instance):
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    search_word = list()
+    for i in instance._queue:
+        file_name = i["nome_do_arquivo"]
+        word_data = list()
+        for line, content in enumerate(i["linhas_do_arquivo"], start=1):
+            if word.lower() in content.lower():
+                word_data.append({
+                    "linha": line,
+                    "conteudo": content
+                })
+
+        if word_data:
+            search_word.append({
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": word_data
+            })
+
+    return search_word
